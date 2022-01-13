cfd05aeef83cf3487914ededeffb5fe326a8b025
hammsika <sivanag.patibandla@gmail.com>
2022-01-13 16:48:38 +0530

    java file added

---
+++ 4ChkEvenOdd.java
@@ -0,0 +1,13 @@
+
+
+class ChkEvenOddnumber
+{
+    public static void main(String args[])
+    {
+        int x=98;
+        if(x % 2 == 0) 
+            System.out.println("Given Number is Even Number");
+        else
+            System.out.println("Given Number is Odd Number");
+    }
+}
